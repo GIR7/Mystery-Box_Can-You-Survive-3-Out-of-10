ad5a95565b4e0a268155c78e196a19fd2348bbf6
GIR7 <0218tyh@gmail.com>
2023-11-24 17:17:23 -0800

    add character

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::time::Instant;
 use mint::Point2;
 
+use ggez::event::KeyCode;
+
 // Define the game state
+#[derive(Clone)]
 pub struct GameState {
     pub player_health: i32,
     pub opened_boxes: usize,
     pub game_start_time: Instant,
+    pub player_position:mint::Point2<f32>,
 }
 
 impl GameState {
@@ -14,6 +18,7 @@ impl GameState {
             player_health: 100,
             opened_boxes: 0,
             game_start_time: Instant::now(),
+            player_position: Point2 { x: 300.0, y: 200.0 },
         }
     }
 }
@@ -32,6 +37,34 @@ impl GameWindow {
         // Clear the screen
         ggez::graphics::clear(ctx, ggez::graphics::Color::BLACK);
 
+        // Render player position
+        // let player_rect = ggez::graphics::Rect::new(
+        //     self.game_state.player_position.x,
+        //     self.game_state.player_position.y,
+        //     20.0, // Replace with your player width
+        //     30.0, // Replace with your player height
+        // );
+        // let player_color = ggez::graphics::Color::WHITE;
+        // let player_mesh = ggez::graphics::Mesh::new_rectangle(
+        //     ctx,
+        //     ggez::graphics::DrawMode::fill(),
+        //     player_rect,
+        //     player_color,
+        // )?;
+        // ggez::graphics::draw(ctx, &player_mesh, ggez::graphics::DrawParam::default())?;
+        let player_rect = ggez::graphics::Mesh::new_rectangle(
+            ctx,
+            ggez::graphics::DrawMode::fill(),
+            ggez::graphics::Rect::new(
+                self.game_state.player_position.x,
+                self.game_state.player_position.y,
+                30.0,
+                30.0,
+            ),
+            ggez::graphics::Color::WHITE,
+        )?;
+        ggez::graphics::draw(ctx, &player_rect, ggez::graphics::DrawParam::default())?;
+        
         // Render player health, opened boxes, and elapsed time
         let text = ggez::graphics::Text::new(format!(
             "Health: {}\nBoxes opened: {}\nTime elapsed: {:?}",
@@ -39,10 +72,9 @@ impl GameWindow {
             self.game_state.opened_boxes,
             self.game_state.game_start_time.elapsed()
         ));
-
         // Draw the text in the center of the screen
         let text_dest = ggez::graphics::DrawParam::new()
-            .dest(Point2 { x: 300.0, y: 200.0 });
+            .dest(Point2 { x:15.0, y: 10.0 });
         ggez::graphics::draw(ctx, &text, text_dest)?;
 
         // Present the frame
@@ -50,3 +82,18 @@ impl GameWindow {
         Ok(())
     }
 }
+
+pub fn handle_input(game_state: &mut GameState, ctx: &mut ggez::Context) {
+        let speed = 5.0; // Adjust as needed
+
+        // Check keyboard input
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Up) {
+            game_state.player_position.y -= speed;
+        }
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Left) {
+            game_state.player_position.x -= speed;
+        }
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Right) {
+            game_state.player_position.x += speed;
+        }
+}    
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use ggez::{event, ContextBuilder, GameResult};
+use ggez::event::KeyCode;
 
 // Import crate
-use Mystery_Box_Can_You_Survive_3_Out_of_10::{GameState, GameWindow};
+use Mystery_Box_Can_You_Survive_3_Out_of_10::{GameState, GameWindow, handle_input};
 
 // Define the event handler for the game
 struct MainState {
     game_window: GameWindow,
+    game_state: GameState,
 }
 
 impl MainState {
     fn new(ctx: &mut ggez::Context) -> GameResult<Self> {
         // Create a new GameState and GameWindow
         let game_state = GameState::new();
-        let game_window = GameWindow::new(game_state);
+        let game_window = GameWindow::new(game_state.clone());
+        // let game_window = GameWindow::new(game_state);
 
-        Ok(MainState { game_window })
+        Ok(MainState { game_window, game_state })
     }
 }
 
 // Implement the event handler for the game
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+        // Handle input
+        handle_input(&mut self.game_state, _ctx);
+        println!("Player Position: {:?}", self.game_state.player_position);
         // Add logic later, first try
         Ok(())
     }
@@ -34,10 +40,13 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
 fn main() -> GameResult {
     // Create a GGEZ context and a window
-    let (ctx, event_loop) =   ContextBuilder::new("Mystery Box", "Y.T.")
+    let (mut ctx, event_loop) = ContextBuilder::new("Mystery Box", "Y.T.")
+        .window_setup(ggez::conf::WindowSetup::default().title("Mystery Box! Can You Survive 3 Out of 10?"))
         .build()?;
+    
     // Create a new instance of MainState
-    let main_state =   MainState { game_window: GameWindow::new(GameState::new()) };
+    let main_state = MainState::new(&mut ctx)?;
+
     // Run the game loop
     event::run(ctx, event_loop, main_state)
 }
