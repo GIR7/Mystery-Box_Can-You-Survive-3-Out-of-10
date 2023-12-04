014363765b44e36aee67e61854d54943de853379
GIR7 <0218tyh@gmail.com>
2023-12-03 22:02:41 -0800

    bg color & health bar

--- src/lib.rs
+++ src/lib.rs
@@ -96,7 +96,7 @@ impl GameWindow {
     }
      pub fn render(&self, ctx: &mut ggez::Context) -> ggez::GameResult {
         // Clear the screen
-        ggez::graphics::clear(ctx, ggez::graphics::Color::BLACK);
+        ggez::graphics::clear(ctx, ggez::graphics::Color::from_rgb(108, 122, 137));
 
         // Render player position
         let player_rect = ggez::graphics::Mesh::new_rectangle(
@@ -112,6 +112,27 @@ impl GameWindow {
         )?;
         ggez::graphics::draw(ctx, &player_rect, ggez::graphics::DrawParam::default())?;
         
+        // Render health bar background (border)
+        let border_color = ggez::graphics::Color::BLACK; // Change to the color you want for the border
+        let health_bar_background_rect = ggez::graphics::Mesh::new_rectangle(
+            ctx,
+            ggez::graphics::DrawMode::stroke(2.0), // Adjust the thickness of the border
+            ggez::graphics::Rect::new(10.0, 10.0, 200.0, 15.0),
+            border_color,
+        )?;
+        ggez::graphics::draw(ctx, &health_bar_background_rect, ggez::graphics::DrawParam::default())?;
+
+        // Render health bar
+        let health_percentage = self.game_state.player_health as f32 / 100.0;
+        let health_bar_width = 200.0 * health_percentage;
+        let health_bar_rect = ggez::graphics::Mesh::new_rectangle(
+            ctx,
+            ggez::graphics::DrawMode::fill(),
+            ggez::graphics::Rect::new(10.0, 10.0, health_bar_width, 15.0),
+            ggez::graphics::Color::GREEN,
+        )?;
+        ggez::graphics::draw(ctx, &health_bar_rect, ggez::graphics::DrawParam::default())?;
+
         // Render player health, opened boxes, and elapsed time
         let text = ggez::graphics::Text::new(format!(
             "Health: {}\nBoxes opened: {}\nTime elapsed: {:?}",
@@ -121,7 +142,8 @@ impl GameWindow {
         ));
         // Draw the text in the center of the screen
         let text_dest = ggez::graphics::DrawParam::new()
-            .dest(Point2 { x:15.0, y: 10.0 });
+            .dest(Point2 { x:15.0, y: 10.0 })
+            .color(ggez::graphics::Color::BLACK);
         ggez::graphics::draw(ctx, &text, text_dest)?;
 
         // Render boxes
