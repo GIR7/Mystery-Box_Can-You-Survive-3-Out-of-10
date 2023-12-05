14b561db1304904c434c14c4777b3cd007add2fc
GIR7 <0218tyh@gmail.com>
2023-12-04 23:01:24 -0800

    freeze the game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // main.rs
-
+use std::time::Instant;
 use ggez::{event, ContextBuilder, GameResult};
 use ggez::event::KeyCode;
 
@@ -24,8 +24,9 @@ impl MainState {
     }
 
     fn check_game_conditions(&mut self, ctx: &mut ggez::Context) {
+        
+        if !self.is_game_over(){
         let game_state = &self.game_window.game_state;
-
         // Check winning conditions
         if game_state.opened_boxes >= 3
             && game_state.player_health > 0
@@ -41,24 +42,34 @@ impl MainState {
             self.game_over = Some(GameOver::Lose);
         }
     }
+    }
+
+    pub fn is_game_over(&self) -> bool {
+        match &self.game_over {
+            Some(GameOver::Win) => true,
+            Some(GameOver::Lose) => true,
+            None => false, // Handle the case where game_over is not set
+        }
+    }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut ggez::Context) -> GameResult {
+        if !self.is_game_over() {
+            // Check game conditions
+            self.check_game_conditions(ctx);
 
-        // Check game conditions
-        self.check_game_conditions(ctx);
-
-        handle_input(&mut self.game_window.game_state, ctx);
-        println!("Player Position: {:?}", self.game_window.game_state.player_position);
-        
+            // Handle input
+            handle_input(&mut self.game_window.game_state, ctx);
+        }
 
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> GameResult {
-        self.game_window.render(ctx)?;
+        
 
+        if self.is_game_over() {
         // If the game is over, display the corresponding message
         if let Some(game_over) = &self.game_over {
             let message = match game_over {
@@ -66,15 +77,21 @@ impl event::EventHandler<ggez::GameError> for MainState {
                 GameOver::Lose => "You Lose!",
             };
 
-            // Draw the message in the center of the screen
-            let text = ggez::graphics::Text::new(message);
-            let text_dest = ggez::graphics::DrawParam::new()
-                .dest(ggez::mint::Point2 { x: 300.0, y: 200.0 });
-            ggez::graphics::draw(ctx, &text, text_dest)?;
+             // Clear the screen
+        ggez::graphics::clear(ctx, ggez::graphics::Color::BLACK);
 
-            // Present the frame
-            ggez::graphics::present(ctx)?;
+            // Draw the message in the center of the screen
+        let text = ggez::graphics::Text::new(message);
+        let text_dest = ggez::graphics::DrawParam::new()
+            .dest(ggez::mint::Point2 { x: 300.0, y: 200.0 });
+        ggez::graphics::draw(ctx, &text, text_dest)?;
+
+        // Present the frame
+        ggez::graphics::present(ctx)?;
         }
+    }else {
+        self.game_window.render(ctx)?;
+    }
         Ok(())
     }
 }
