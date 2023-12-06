ccaef6506dc0837aceebaf4be9f8ea74ad3c1e3a
GIR7 <0218tyh@gmail.com>
2023-12-05 20:31:41 -0800

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
-use std::time::Instant;
-use mint::Point2;
 use ggez::event::KeyCode;
+use mint::Point2;
 use rand::Rng;
-
-// Define the game state
-#[derive(Clone)]
-pub struct GameState {
-    pub player_health: i32,
-    pub opened_boxes: usize,
-    pub game_start_time: Instant,
-    pub player_position:mint::Point2<f32>,
-    pub boxes: Vec<GameBox>,
-}
+use std::time::Instant;
 
 // Define the effect of opening a box
 #[derive(Clone, Copy)]
@@ -27,6 +17,16 @@ pub struct GameBox {
     pub position: Point2<f32>,
     pub effect: BoxEffect,
     pub opened: bool,
+}
+
+// Define the game state
+#[derive(Clone)]
+pub struct GameState {
+    pub player_health: i32,
+    pub opened_boxes: usize,
+    pub game_start_time: Instant,
+    pub player_position: mint::Point2<f32>,
+    pub boxes: Vec<GameBox>,
 }
 
 impl GameState {
@@ -46,7 +46,7 @@ impl GameState {
                 };
 
                 // Check if the potential position collides with any existing box
-                let collides = boxes.iter().any(|existing_box:&GameBox| {
+                let collides = boxes.iter().any(|existing_box: &GameBox| {
                     (box_position.x + 30.0 > existing_box.position.x)
                         && (existing_box.position.x + 30.0 > box_position.x)
                         && (box_position.y + 30.0 > existing_box.position.y)
@@ -60,8 +60,8 @@ impl GameState {
 
             let box_effect = match rng.gen_range(0..2) {
                 0 => BoxEffect::Cure(rng.gen_range(5..50)), // Add between 5 and 50 points
-                1 => BoxEffect::Injury(rng.gen_range(50..100)),    // Deduct between 50 and 100 points
-                _ => BoxEffect::NoEffect,//nothing happen
+                1 => BoxEffect::Injury(rng.gen_range(50..100)), // Deduct between 50 and 100 points
+                _ => BoxEffect::NoEffect,                   //nothing happen
             };
 
             boxes.push(GameBox {
@@ -70,7 +70,6 @@ impl GameState {
                 opened: false,
             });
         }
-
 
         GameState {
             player_health: 100,
@@ -83,7 +82,6 @@ impl GameState {
 }
 
 // Define the game window
-
 pub struct GameWindow {
     pub game_state: GameState,
 }
@@ -94,7 +92,7 @@ impl GameWindow {
             game_state: GameState::new(),
         }
     }
-     pub fn render(&self, ctx: &mut ggez::Context) -> ggez::GameResult {
+    pub fn render(&self, ctx: &mut ggez::Context) -> ggez::GameResult {
         // Clear the screen
         ggez::graphics::clear(ctx, ggez::graphics::Color::from_rgb(108, 122, 137));
 
@@ -111,7 +109,7 @@ impl GameWindow {
             ggez::graphics::Color::WHITE,
         )?;
         ggez::graphics::draw(ctx, &player_rect, ggez::graphics::DrawParam::default())?;
-        
+
         // Render health bar background (border)
         let border_color = ggez::graphics::Color::BLACK; // Change to the color you want for the border
         let health_bar_background_rect = ggez::graphics::Mesh::new_rectangle(
@@ -120,7 +118,11 @@ impl GameWindow {
             ggez::graphics::Rect::new(10.0, 10.0, 200.0, 15.0),
             border_color,
         )?;
-        ggez::graphics::draw(ctx, &health_bar_background_rect, ggez::graphics::DrawParam::default())?;
+        ggez::graphics::draw(
+            ctx,
+            &health_bar_background_rect,
+            ggez::graphics::DrawParam::default(),
+        )?;
 
         // Render health bar
         let health_percentage = self.game_state.player_health as f32 / 100.0;
@@ -142,7 +144,7 @@ impl GameWindow {
         ));
         // Draw the text on the screen
         let text_dest = ggez::graphics::DrawParam::new()
-            .dest(Point2 { x:15.0, y: 10.0 })
+            .dest(Point2 { x: 15.0, y: 10.0 })
             .color(ggez::graphics::Color::BLACK);
         ggez::graphics::draw(ctx, &text, text_dest)?;
 
@@ -150,8 +152,8 @@ impl GameWindow {
         let fov_rect = ggez::graphics::Rect::new(
             self.game_state.player_position.x - 100.0, // Adjust the width of the FOV rectangle
             self.game_state.player_position.y - 100.0, // Adjust the height of the FOV rectangle
-            200.0, // Width of the FOV rectangle
-            200.0, // Height of the FOV rectangle
+            200.0,                                     // Width of the FOV rectangle
+            200.0,                                     // Height of the FOV rectangle
         );
 
         // Iterate over the boxes and render only those within the FOV
@@ -182,36 +184,38 @@ impl GameWindow {
 }
 
 pub fn handle_input(game_state: &mut GameState, ctx: &mut ggez::Context) {
-        let speed = 1.0; 
-        // Check keyboard input
-        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Up) {
-            game_state.player_position.y -= speed;
-        }
-        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Down) {
-            game_state.player_position.y += speed;
-        }
-        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Left) {
-            game_state.player_position.x -= speed;
-        }
-        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Right) {
-            game_state.player_position.x += speed;
-        }
+    let speed = 2.0;
+    // Check keyboard input
+    if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Up) {
+        game_state.player_position.y -= speed;
+    }
+    if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Down) {
+        game_state.player_position.y += speed;
+    }
+    if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Left) {
+        game_state.player_position.x -= speed;
+    }
+    if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Right) {
+        game_state.player_position.x += speed;
+    }
 
-        // Check for box interactions
+    // Check for box interactions
     for box_entity in &mut game_state.boxes {
-        if player_near_box(&game_state.player_position, &box_entity.position) && !box_entity.opened {
+        if player_near_box(&game_state.player_position, &box_entity.position) && !box_entity.opened
+        {
             if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Space) {
                 // Open the box and apply its effect to player health
                 match box_entity.effect {
-                    BoxEffect::Injury(points) => game_state.player_health =std::cmp::max(
-                        game_state.player_health - points,
-                        0,
-                    ), 
+                    //prevent health below 0
+                    BoxEffect::Injury(points) => {
+                        game_state.player_health =
+                            std::cmp::max(game_state.player_health - points, 0)
+                    }
                     // Ensure health doesn't exceed 100
-                    BoxEffect::Cure(points) => game_state.player_health = std::cmp::min(
-                        game_state.player_health + points,
-                        100,
-                    ), 
+                    BoxEffect::Cure(points) => {
+                        game_state.player_health =
+                            std::cmp::min(game_state.player_health + points, 100)
+                    }
                     BoxEffect::NoEffect => (),
                 }
                 game_state.opened_boxes += 1;
@@ -219,13 +223,12 @@ pub fn handle_input(game_state: &mut GameState, ctx: &mut ggez::Context) {
             }
         }
     }
-
-}    
+}
 
 fn player_near_box(player_position: &Point2<f32>, box_position: &Point2<f32>) -> bool {
     // Simple distance check for player proximity to a box
     let distance = ((player_position.x - box_position.x).powi(2)
         + (player_position.y - box_position.y).powi(2))
     .sqrt();
-    distance < 30.0 // Adjust this value based on your preference
+    distance < 30.0 // fits current setup
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,44 @@
 // main.rs
-use std::time::Instant;
+
 use ggez::{event, ContextBuilder, GameResult};
-use ggez::event::KeyCode;
 
-
-use Mystery_Box_Can_You_Survive_3_Out_of_10::{GameState, GameWindow, handle_input};
+use mystery_box_can_you_survive_3_out_of_10::{handle_input, GameWindow};
 
 struct MainState {
     game_window: GameWindow,
     game_over: Option<GameOver>,
 }
-
 enum GameOver {
     Win,
     Lose,
 }
-
 impl MainState {
     fn new(_ctx: &mut ggez::Context) -> GameResult<Self> {
         let game_window = GameWindow::new();
 
-        Ok(MainState { game_window,game_over: None, })
+        Ok(MainState {
+            game_window,
+            game_over: None,
+        })
     }
 
-    fn check_game_conditions(&mut self, ctx: &mut ggez::Context) {
-        
-        if !self.is_game_over(){
-        let game_state = &self.game_window.game_state;
-        // Check winning conditions
-        if game_state.opened_boxes >= 3
-            && game_state.player_health > 0
-            && game_state.game_start_time.elapsed().as_secs() <= 60
-        {
-            self.game_over = Some(GameOver::Win);
-        }
+    fn check_game_conditions(&mut self, _ctx: &mut ggez::Context) {
+        if !self.is_game_over() {
+            let game_state = &self.game_window.game_state;
+            // Check winning conditions
+            if game_state.opened_boxes >= 3
+                && game_state.player_health > 0
+                && game_state.game_start_time.elapsed().as_secs() <= 60
+            {
+                self.game_over = Some(GameOver::Win);
+            }
 
-        // Check losing conditions
-        if game_state.player_health <= 0
-            || game_state.game_start_time.elapsed().as_secs() > 60
-        {
-            self.game_over = Some(GameOver::Lose);
+            // Check losing conditions
+            if game_state.player_health <= 0 || game_state.game_start_time.elapsed().as_secs() > 60
+            {
+                self.game_over = Some(GameOver::Lose);
+            }
         }
-    }
     }
 
     pub fn is_game_over(&self) -> bool {
@@ -67,38 +64,38 @@ impl event::EventHandler<ggez::GameError> for MainState {
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> GameResult {
-        
-
         if self.is_game_over() {
-        // If the game is over, display the corresponding message
-        if let Some(game_over) = &self.game_over {
-            let message = match game_over {
-                GameOver::Win => "You Win!",
-                GameOver::Lose => "You Lose!",
-            };
+            // If the game is over, display the corresponding message
+            if let Some(game_over) = &self.game_over {
+                let message = match game_over {
+                    GameOver::Win => "You Win!",
+                    GameOver::Lose => "You Lose!",
+                };
 
-             // Clear the screen
-        ggez::graphics::clear(ctx, ggez::graphics::Color::BLACK);
+                // Clear the screen
+                ggez::graphics::clear(ctx, ggez::graphics::Color::BLACK);
 
-            // Draw the message in the center of the screen
-        let text = ggez::graphics::Text::new(message);
-        let text_dest = ggez::graphics::DrawParam::new()
-            .dest(ggez::mint::Point2 { x: 300.0, y: 200.0 });
-        ggez::graphics::draw(ctx, &text, text_dest)?;
+                // Draw the message in the center of the screen
+                let text = ggez::graphics::Text::new(message);
+                let text_dest = ggez::graphics::DrawParam::new()
+                    .dest(ggez::mint::Point2 { x: 300.0, y: 200.0 });
+                ggez::graphics::draw(ctx, &text, text_dest)?;
 
-        // Present the frame
-        ggez::graphics::present(ctx)?;
+                // Present the frame
+                ggez::graphics::present(ctx)?;
+            }
+        } else {
+            self.game_window.render(ctx)?;
         }
-    }else {
-        self.game_window.render(ctx)?;
-    }
         Ok(())
     }
 }
 
 fn main() -> GameResult {
     let (mut ctx, event_loop) = ContextBuilder::new("Mystery Box", "Y.T.")
-        .window_setup(ggez::conf::WindowSetup::default().title("Mystery Box! Can You Survive 3 Out of 10?"))
+        .window_setup(
+            ggez::conf::WindowSetup::default().title("Mystery Box! Can You Survive 3 Out of 10?"),
+        )
         .build()?;
 
     let main_state = MainState::new(&mut ctx)?;
